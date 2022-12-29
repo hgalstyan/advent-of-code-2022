eaf2d0556c241fde90e95b2c5d08ae31f0db3571
Ovannes Galstian <galstyan.hovhannes@gmail.com>
2022-12-28 16:33:18 -0800

    Added day4. Renamed files

--- day1/day1.py
+++
@@ -1,6 +0,0 @@
-data = open("input.txt", "r").read()
-
-calories = [sum(map(int, elf.split("\n"))) for elf in data.split("\n\n")]
-
-print("Answer 1: ", max(calories))
-print("Answer 2: ", sum(sorted(calories)[-3:]))
