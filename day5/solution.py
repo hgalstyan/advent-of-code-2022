94df62c37734e76049f464afd59d75aad55a670d
Ovannes Galstian <galstyan.hovhannes@gmail.com>
2022-12-28 19:04:48 -0800

    Added day 5

--- day5/solution.py
+++ day5/solution.py
@@ -1,9 +1,33 @@
-data = open("test.txt", "r").read().split("\n")
-origin, moves = data[:data.index('')], data[data.index('')+1:]
-origin = [o.replace("[", "").replace("]", "").replace(" ", "#") for o in origin]
-print(origin)
-print(moves)
+data = open("input.txt", "r").read().split("\n")
 
-for line in moves:
+sep = [i for i, v in enumerate(data) if v == ""][0]
+
+crates = data[: sep - 1][::-1]
+procedures = data[sep + 1 :]
+
+stack_num = max(map(int, data[sep - 1].split()))
+stacks1 = [[] for _ in range(stack_num + 1)]
+stacks2 = [[] for _ in range(stack_num + 1)]
+
+for line in crates:
+    items = [line[i] for i in range(1, len(line), 4)]
+    for i, v in enumerate(items):
+        if v != " ":
+            stacks1[i + 1].append(v)
+            stacks2[i + 1].append(v)
+
+
+for line in procedures:
     _, move, _, _from, _, to = [int(v) if i % 2 else v for i, v in enumerate(line.split())]
-    print(move,_from,to)
+    
+    moving_crates1 = stacks1[_from][-move:]
+    moving_crates1 = moving_crates1[::-1]
+    stacks1[to].extend(moving_crates1)
+    del stacks1[_from][-move:]
+
+    moving_crates2 = stacks2[_from][-move:]
+    stacks2[to].extend(moving_crates2)
+    del stacks2[_from][-move:]
+
+print("Answer 1: ", "".join(stacks1[i][-1] for i in range(1, len(stacks1))))
+print("Answer 2: ", "".join(stacks2[i][-1] for i in range(1, len(stacks2))))
